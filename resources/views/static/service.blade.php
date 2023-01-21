1fb7e39b98668094affd5f99154a68c3a303f225
shimazaki17 <32214011+shimazaki17@users.noreply.github.com>
2023-01-21 09:18:39 +0900

    Update service.blade.php

--- resources/views/static/service.blade.php
+++ resources/views/static/service.blade.php
@@ -7,31 +7,24 @@
 @section('content')
     <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-y-12 gap-x-8">
         <div class="max-w-xs border-8 border-white">
-            <p class="p-6 border-b border-black border-dashed text-lg text-center font-bold">企業法務</p>
+            <p class="p-6 border-b border-black border-dashed text-lg text-center font-bold">企業関連の業務</p>
             <div class="px-4 py-6 text-xs">
-                ・訴訟代理人<br />
-                ・法務相談<br />
-                ・契約書作成
+                ・規程作成<br />
+                ・法令調査<br />
+                ・意見書作成<br />
+                ・訴訟代理<br />
+                ・紛争解決
             </div>
         </div>
         <div class="max-w-xs border-8 border-white">
-            <p class="p-6 mb-6 border-b border-black border-dashed text-lg text-center font-bold">債権回収</p>
+            <p class="p-6 mb-6 border-b border-black border-dashed text-lg text-center font-bold">個人関連の業務</p>
             <div class="px-4 py-6 text-xs">
-                交渉、内容証明郵便による催促、訴訟等
+                ・家族関係
+                ・刑事事件
+                ・債権回収
+                ・SNS被害
             </div>
         </div>
-        <div class="max-w-xs border-8 border-white">
-            <p class="p-6 mb-6 border-b border-black border-dashed text-lg text-center font-bold">相続</p>
-            <div class="px-4 py-6 text-xs">
-                遺産分割協議
-            </div>
-        </div>
-        <div class="max-w-xs border-8 border-white">
-            <p class="p-6 mb-6 border-b border-black border-dashed text-lg text-center font-bold">SNS名誉毀損</p>
-            <div class="px-4 py-6 text-xs">
-                ・発信者情報の開示<br />
-                ・投稿の削除
-            </div>
-        </div>
+
     </div>
 @endsection
