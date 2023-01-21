07e040c94c3344b72dbd2db16ff35d4f49693b57
shimazaki17 <32214011+shimazaki17@users.noreply.github.com>
2023-01-21 10:00:36 +0900

    Update service.blade.php

--- resources/views/static/service.blade.php
+++ resources/views/static/service.blade.php
@@ -17,10 +17,20 @@
         </div>
        
         <div class="max-w-xs border-8 border-white">
-            <p class="p-6 border-b border-black border-dashed text-lg text-center font-bold">法律顧問、アウトソース</p>
+            <p class="p-6 border-b border-black border-dashed text-lg text-center font-bold">法律顧問</p>
             <div class="px-4 py-6 text-xs">
                 日々の法的な疑問やトラブル、改正法対応などに対応します。<br />
                 これまで複数の外資・内資の企業において経営者や法務部に寄り添った法務サポートを提供してきました。<br />
+                
+                ・顧問先は優先対応とし、24時間以内に対応いたします。<br />
+                ・一定時間内のご相談を、顧問料の枠内で対応いたします。<br />
+                ・顧問料の範囲を超える場合、タイムチャージについてディスカウントを設定します。
+            </div>
+        </div> 
+        
+        <div class="max-w-xs border-8 border-white">
+            <p class="p-6 border-b border-black border-dashed text-lg text-center font-bold">法務アウトソース</p>
+            <div class="px-4 py-6 text-xs">
                 一時的に法務人員が足りない場合など、法務機能のアウトソース先としてご支援させてください。
             </div>
         </div>     
